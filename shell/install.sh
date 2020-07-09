4b86388ac9ac071650ed5e9cbaafecb9dc9879f7
mlboy <mlboy@126.com>
2020-07-10 00:36:12 +0800

    friendly

--- shell/install.sh
+++ shell/install.sh
@@ -1,66 +1,77 @@
 #!/bin/bash
 
 
-tmp=no
-read -p "Install Env [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+opt_env=no
+read -p "Install Env [y/n]: " opt_env
+
+opt_mysql=no
+read -p "Install MySQL [y/n]: " opt_mysql
+
+opt_etcd=no
+read -p "Install Etcd [y/n]: " opt_etcd
+
+opt_prom=no
+read -p "Install Prometheus [y/n]: " opt_prom
+
+opt_graf_db=no
+read -p "Install Grafana Database [y/n]: " opt_graf_db
+
+opt_graf=no
+read -p "Install Grafana [y/n]: " opt_graf
+
+opt_juno_db=no
+read -p "Install Juno Database [y/n]: " opt_juno_db
+
+opt_juno_mock=no
+read -p "Install Juno Mock [y/n]: " opt_juno_mock
+
+opt_juno_admin=no
+read -p "Install Juno Admin [y/n]: " opt_juno_admin
+
+install=no
+read -p "Start the installation [y/n]: " install
+
+if [ "$install" != "y" ];then
+  exit
+fi
+
+
+if [ "$opt_env" == "y" ];then
 ./install_dir.sh
 ./install_env.sh
 ./install_juno.sh
 fi
 
-
-tmp=no
-read -p "Install MySQL [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_mysql" == "y" ];then
   ./install_mysql.sh
   sleep 2
 fi
 
-tmp=no
-read -p "Install Etcd [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_etcd" == "y" ];then
   ./install_etcd.sh
 fi
 
-tmp=no
-read -p "Install Prometheus [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_prom" == "y" ];then
   ./install_prometheus.sh
 fi
 
-
-tmp=no
-read -p "Install Grafana Database [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_graf_db" == "y" ];then
   ./install_grafana_database.sh
 fi
 
-
-tmp=no
-read -p "Install Grafana [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_graf" == "y" ];then
   ./install_grafana.sh
 fi
 
-
-tmp=no
-read -p "Install Juno Database [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_juno_db" == "y" ];then
   ./install_juno_database.sh
 fi
 
-
-tmp=no
-read -p "Install Juno Mock [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_juno_mock" == "y" ];then
   ./install_juno_mock.sh
 fi
 
-
-tmp=no
-read -p "Install Juno Admin [y/n]: " tmp
-if [ "$tmp" == "y" ];then
+if [ "$opt_juno_admin" == "y" ];then
   ./install_juno_admin.sh
 fi
 
