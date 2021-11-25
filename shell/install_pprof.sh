35297b34d122cff787a09eeae39a3f94ea382570
Denis Yin <37320260+DenisYin66@users.noreply.github.com>
2021-11-25 14:34:31 +0800

    add --no-check-certificate for www2

--- shell/install_pprof.sh
+++ shell/install_pprof.sh
@@ -8,7 +8,7 @@ mkdir -p ${APP_PATH}
 mkdir -p ${APP_PATH}/graphviz
 chown -R www:www ${APP_PATH}
 
-wget -P ${DOWNLOAD_PATH}/${APP_NAME}  https://www2.graphviz.org/Packages/stable/portable_source/graphviz-2.44.0.tar.gz
+wget -P ${DOWNLOAD_PATH}/${APP_NAME} --no-check-certificate https://www2.graphviz.org/Packages/stable/portable_source/graphviz-2.44.0.tar.gz
 cd ${DOWNLOAD_PATH}/${APP_NAME} && tar zxvf graphviz-2.44.0.tar.gz
 cd ${DOWNLOAD_PATH}/${APP_NAME}/graphviz-2.44.0 && ./configure --prefix=${APP_PATH}/graphviz
 cd ${DOWNLOAD_PATH}/${APP_NAME}/graphviz-2.44.0 && make && make install
